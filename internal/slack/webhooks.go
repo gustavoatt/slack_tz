e187281e30685e610e5223cf8a403e1fd74b951d
Gustavo Torres <gustavo.torres@airbnb.com>
2020-11-23 11:49:54 -0600

    Fix linter issues

--- internal/slack/webhooks.go
+++ internal/slack/webhooks.go
@@ -5,12 +5,12 @@ type HookRequest struct {
 	Command     string `json:"command"`
 	Text        string `json:"text"`
 	ResponseURL string `json:"response_url"`
-	TriggerId   string `json:"trigger_id"`
-	UserId      string `json:"user_id"`
+	TriggerID   string `json:"trigger_id"`
+	UserID      string `json:"user_id"`
 	UserName    string `json:"user_name"`
 	// Context about Slack Workspace
-	TeamId       string `json:"team_id"`
-	EnterpriseId string `json:"enterprise_id"`
+	TeamID       string `json:"team_id"`
+	EnterpriseID string `json:"enterprise_id"`
 	TeamDomain   string `json:"team_domain"`
-	ApiAppId     string `json:"api_app_id"`
+	APIAppID     string `json:"api_app_id"`
 }
